5931e85cc01a778ef19fc43335bc3545f3b1c63b
Victor Blanco <vblanco20-1@hotmail.com>
2018-06-04 19:54:24 +0200

    First commit

---
+++ AquilaEngine/include/PrecompiledHeader.h
@@ -0,0 +1,31 @@
+#pragma once
+
+// Link library dependencies
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "winmm.lib")
+
+// System includes
+#include <windows.h>
+
+// DirectX includes
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+
+// STL includes
+#include <iostream>
+#include <string>
+#include <algorithm>
+// Safely release a COM object.
+template<typename T>
+inline void SafeRelease(T& ptr)
+{
+	if (ptr != NULL)
+	{
+		ptr->Release();
+		ptr = NULL;
+	}
+}
